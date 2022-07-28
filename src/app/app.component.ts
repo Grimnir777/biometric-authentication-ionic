d07289dbb109913dc2f163718d00e3f3739b235c
Thibaud Germain <thibaud.germain1@ibm.com>
2022-07-28 19:09:51 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {BiometricWrapper} from '@awesome-cordova-plugins/biometric-wrapper/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private biometricWrapper: BiometricWrapper) {
+    this.biometricWrapper.activateIris({'PID_XML': '&lt;pid-xml/>'})
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
+  }
 }
